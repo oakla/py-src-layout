972d51f7040ed172410682f7fbf8e422622b71d0
oakleal <Alexander.Oakley@cba.com.au>
2023-06-20 21:46:31 +1000

    improve

--- src/pkgname/__main__.py
+++ src/pkgname/__main__.py
@@ -13,12 +13,21 @@ if sys.version_info >= (3, 10):
 else: 
     from importlib_resources import files
 
+
+#* Demonstrate accessing datafiles at runtime
+
+# Read text from a file
 example_text = files('pkgname.data_files').joinpath('example.txt').read_text()
 print(example_text)
 
+# Read binary data from a file
 import json
-example_map = json.loads(files('pkgname').joinpath('example2.json').read_text())
-print(f"Hello {example_map['Hello']} from json file")
+example_map = json.loads(files('pkgname').joinpath('example2.json').read_bytes())
+print(f"Hello {example_map['Hello']} from json file\n")
 
 example_path = files('pkgname').joinpath('another_data_file.html')
-print(example_path)
+print(f"html file path is {example_path}")
+
+# In some cases you may need to make changes to the config files and/or
+# use a MANIFEST.in file to include additional files.
+# https://setuptools.pypa.io/en/latest/userguide/datafiles.html#data-files-support 
