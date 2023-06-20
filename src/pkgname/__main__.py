bfc19750284c289a13158a7a668af799436f836a
oakleal <Alexander.Oakley@cba.com.au>
2023-06-20 21:10:53 +1000

    improve

--- src/pkgname/__main__.py
+++ src/pkgname/__main__.py
@@ -2,4 +2,16 @@
 This module is the default starting point of your package after installation.
 """
 
+import sys
+# importlib_resources is a backport of importlib.resources which provides
+# the newest features of importlib.resources to older Python versions.
+
+# You could just use importlib_resources regardless of the Python version.
+# Go ahead and change it, but remember to update the dependencies list.
+if sys.version_info >= (3, 10):
+    from importlib.resources import files
+else: 
+    from importlib_resources import files
+
+
 
